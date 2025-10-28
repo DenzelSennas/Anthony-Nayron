bde9e68802433d3e04ddb64b31e6c10704d05e91
FreireCF <clecioferreira011@gmail.com>
2025-10-28 15:44:41 -0300

    adicionando arquivos de conexao com o BD

--- projeto-banco-dados/config.php
+++ projeto-banco-dados/config.php
@@ -1,8 +1,9 @@
 <?php
-  $dbHost = "localhost";
-  $dbUser = "root";
-  $dbPass = "sql1050";
-  $dbName = "formulario";
+  // configurações de conexão do bd
+  $dbHost = ""; //se for pelo xampp, use localhost
+  $dbUser = ""; //geralmente é root
+  $dbPass = ""; //senha do sql na sua máquina
+  $dbName = "formulario"; //eu criei o bd com esse nome mas pode ser alterado
 
   $conexao = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
 
